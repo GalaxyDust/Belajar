a96520d93f3711e61a821a65aafb5218664b6833
Maulana Yusuf <74699755+GalaxyDust@users.noreply.github.com>
2020-12-05 17:01:10 +0700

    Add files via upload

---
+++ PHP PENGULANGAN.php
@@ -0,0 +1,42 @@
+<!-- <?php 
+// for
+// while
+// do..while
+// foreach = pengulangan array
+
+// for ($i=0; $i < 2; $i++) { 
+// 	echo "Assalamualaikum <br>";
+// }
+	
+	// $i=0;
+	// while ( $i<2) {
+	// 	echo "Assalamualaikum <br>";
+	// $i++;
+	// }	
+
+
+		// $i=0;
+		// do {
+		// 	echo "Assalamualaikum <br>";
+		// $i++;
+		// } while ($i<2);
+?> -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan 1</title>
+</head>
+<body>
+
+<table border="1" cellpadding="10" cellspacing="0">
+	<?php for ($i=1; $i <=3; $i++) : ?>
+		<tr>
+			<?php for ($j=1; $j <=5 ; $j++) : ?>
+				<td><?= "$i,$j"; ?></td>
+			<?php endfor; ?>
+		</tr>
+	<?php endfor; ?>
+</table>
+
+</body>
+</html>
